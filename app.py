cb3bd777c67191e22f87233468403d3d60a3e608
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-12-23 00:56:26 +0200

    commit

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ def generate_frames():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route('/')
 def home():
-    return render_template("index.html")   
+    return render_template("index.html")
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')    
 if __name__ == '__main__':
     app.run(debug=True)
